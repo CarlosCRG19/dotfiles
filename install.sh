ed38117ec72c0d82d6537ff210bea89614b9c803
CarlosCRG19 <carloscrg19@gmail.com>
2024-06-14 19:31:36 -0600

    updated installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+OS="$(uname -s)"
 
+# Common operations for Linux and macOS
 rm -rf "$XDG_CONFIG_HOME/nvim"
 ln -s "$DOTFILES/nvim" "$XDG_CONFIG_HOME"
-
-rm -rf "$XDG_CONFIG_HOME/i3"
-ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
 rm -rf "$XDG_CONFIG_HOME/alacritty"
 ln -s "$DOTFILES/alacritty" "$XDG_CONFIG_HOME"
@@ -18,5 +15,23 @@ ln -s "$DOTFILES/zsh" "$XDG_CONFIG_HOME"
 rm -rf "$HOME/.zshenv"
 ln -s "$DOTFILES/zsh/.zshenv" "$HOME"
 
-mkdir -p "$XDG_DATA_HOME"
-cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME/fonts"
+case "$OS" in
+    Linux)
+        rm -rf "$XDG_CONFIG_HOME/X11"
+        ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+
+        rm -rf "$XDG_CONFIG_HOME/i3"
+        ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+
+        mkdir -p "$XDG_DATA_HOME"
+        cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME/fonts"
+        ;;
+    Darwin)
+        echo "macOS detected, no additional operations needed."
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
